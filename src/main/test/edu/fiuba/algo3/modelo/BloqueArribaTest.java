22d859e84ea8c8c3cd47f9c171b8602eaf8dd422
Branko Ivan Tintilay Tacacho <btintilay@fi.uba.ar>
2020-12-15 01:22:56 -0300

    Agrego prueba de BloqueArriba

--- src/main/test/edu/fiuba/algo3/modelo/BloqueArribaTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/BloqueArribaTest.java
@@ -2,8 +2,18 @@ package edu.fiuba.algo3.modelo;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class BloqueArribaTest {
 
+    @Test
+    public void test01PersonajeEjecutaBloqueYSuPosicionEsLaCorrecta(){
+        Personaje personaje = new Personaje();
+        BloqueArriba bloque = new BloqueArriba();
+        Posicion posicion = new Posicion(0,1);
+
+        personaje.ejecutarBloque(bloque);
+
+        assertTrue(personaje.obtenerPosicion().compararPosiciones(posicion));
+    }
 }
