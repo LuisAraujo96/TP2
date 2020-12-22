2174a9ab9dd56824525d40840963b65433c79a38
LuisAraujo96 <luis_araujo96@outlook.com>
2020-12-22 20:42:35 -0300

    feat: se agrego una matriz de posiciones
    *la matriz es una matriz cuadrada de posiciones que crea al Personaje en su centro
    *la matriz se maneja con filas y columnas
    *se le puede consultar la posicion del personaje
    *sabe actualizar la posicion del personaje
    *sabe dibujar la posicion en la que se encuentra el personaje

--- src/main/java/edu/fiuba/algo3/modelo/SectorDibujo.java
+++ src/main/java/edu/fiuba/algo3/modelo/SectorDibujo.java
@@ -1,6 +1,45 @@
 package edu.fiuba.algo3.modelo;
 
 public class SectorDibujo {
+    private int extension;
+    private Posicion posicion[][];
+    private int filaPersonaje;
+    private int columnaPersonaje;
+    private int calculoAuxiliarPositivo;
+    private int calculoAuxiliarNegativo;
+
+    public SectorDibujo(){
+        this.extension = 101; //Numeros impares solamente
+        this.calculoAuxiliarPositivo = (this.extension / 2);
+        this.calculoAuxiliarNegativo = (this.extension / 2) * (-1);
+
+        this.posicion = new Posicion[this.extension][this.extension];
+
+        for(int fila = 0; fila < this.extension; fila++){
+            for(int columna = 0; columna < this.extension; columna++){
+                this.posicion[fila][columna] = new Posicion(this.calculoAuxiliarNegativo + columna, this.calculoAuxiliarPositivo - fila);
+            }
+        }
+
+        this.filaPersonaje = (this.extension / 2);
+        this.columnaPersonaje = (this.extension / 2);
+    }
+
+    public Posicion obtenerPosicion() {
+        return this.posicion[this.filaPersonaje][this.columnaPersonaje];
+    }
+
+    public int dibujar(Lapiz unLapiz){
+        return unLapiz.utilizarSobre(this.posicion[this.filaPersonaje][this.columnaPersonaje]);
+    }
+
+    public void moverseHaciaArriba(){ this.filaPersonaje -= 1; }
+
+    public void moverseHaciaAbajo(){ this.filaPersonaje += 1; }
+
+    public void moverseHaciaDerecha() { this.columnaPersonaje += 1; }
+
+    public void moverseHaciaIzquierda() { this.columnaPersonaje -= 1; }
 
     //Deberia estar compuesto por una matriz donde cada posicion este llena de 0's,
     //donde el 0 significa que es una posicion la cual no esta pintada o pintada de blanco
