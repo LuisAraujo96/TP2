aaccf13afc12744f3a09685d10b1f8656718231a
Branko Ivan Tintilay Tacacho <btintilay@fi.uba.ar>
2021-01-16 22:43:11 -0300

    Realizo cambios en el Sector Dibujo

--- src/main/java/edu/fiuba/algo3/modelo/SectorDibujo.java
+++ src/main/java/edu/fiuba/algo3/modelo/SectorDibujo.java
@@ -2,28 +2,27 @@ package edu.fiuba.algo3.modelo;
 
 public class SectorDibujo {
     private Posicion matrizDePosiciones[][];
+    private int extensionDeLaMatriz = 11;
     private int yDelPersonaje;
     private int xDelPersonaje;
 
     public SectorDibujo(){
-        int extensionDeLaMatriz = 101; //Matriz de 101x101. Usar numeros impares para la matriz unicamente
-        int extremoYPositivo = (extensionDeLaMatriz / 2); //Calculo que sirve para que cada posicion de la matriz tenga un x e y correcto
-        int extremoXNegativo = (extensionDeLaMatriz / 2) * (-1); //Calculo que sirve para que cada posicion de la matriz tenga un x e y correcto
+        int ejeMedio = (extensionDeLaMatriz / 2); //Calculo que sirve para que cada posicion de la matriz tenga un x e y correcto
 
         this.matrizDePosiciones = new Posicion[extensionDeLaMatriz][extensionDeLaMatriz];
 
         for(int fila = 0; fila < extensionDeLaMatriz; fila++){
             for(int columna = 0; columna < extensionDeLaMatriz; columna++){
-                this.matrizDePosiciones[fila][columna] = new Posicion(extremoXNegativo + columna, extremoYPositivo - fila);
+                this.matrizDePosiciones[columna][fila] = new Posicion(columna - ejeMedio, ejeMedio - fila);
             }
         }
 
-        this.yDelPersonaje = (extensionDeLaMatriz / 2);
-        this.xDelPersonaje = (extensionDeLaMatriz / 2);
+        this.yDelPersonaje = ejeMedio;
+        this.xDelPersonaje = ejeMedio;
     }
 
     public Posicion obtenerPosicionDelPersonaje() {
-        return this.matrizDePosiciones[this.yDelPersonaje][this.xDelPersonaje];
+        return this.matrizDePosiciones[this.xDelPersonaje][this.yDelPersonaje];
     }
 
     public void moverPersonajeA(Direccion unaDireccion) {
