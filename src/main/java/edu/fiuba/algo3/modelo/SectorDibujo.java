7fb8c10461cb7589edd7e7a08832c3b80f831071
LuisAraujo96 <luis_araujo96@outlook.com>
2021-01-19 22:17:56 -0300

    fix: el sector dibujo ahora posee una matriz circular por la cual el personaje puede moverse

--- src/main/java/edu/fiuba/algo3/modelo/SectorDibujo.java
+++ src/main/java/edu/fiuba/algo3/modelo/SectorDibujo.java
@@ -1,34 +1,35 @@
 package edu.fiuba.algo3.modelo;
 
 public class SectorDibujo {
+    private int extensionDeLaMatriz;
     private Posicion matrizDePosiciones[][];
     private int yDelPersonaje;
     private int xDelPersonaje;
 
     public SectorDibujo(){
-        int extensionDeLaMatriz = 101; //Matriz de 101x101. Usar numeros impares para la matriz unicamente
-        int extremoYPositivo = (extensionDeLaMatriz / 2); //Calculo que sirve para que cada posicion de la matriz tenga un x e y correcto
-        int extremoXNegativo = (extensionDeLaMatriz / 2) * (-1); //Calculo que sirve para que cada posicion de la matriz tenga un x e y correcto
+        this.extensionDeLaMatriz = 101; //Matriz de 101x101. Usar numeros impares para la matriz unicamente
+        int extremoYPositivo = (this.extensionDeLaMatriz / 2); //Calculo que sirve para que cada posicion de la matriz tenga un x e y correcto
+        int extremoXNegativo = (this.extensionDeLaMatriz / 2) * (-1); //Calculo que sirve para que cada posicion de la matriz tenga un x e y correcto
 
         this.matrizDePosiciones = new Posicion[extensionDeLaMatriz][extensionDeLaMatriz];
 
-        for(int fila = 0; fila < extensionDeLaMatriz; fila++){
-            for(int columna = 0; columna < extensionDeLaMatriz; columna++){
+        for(int fila = 0; fila < this.extensionDeLaMatriz; fila++){
+            for(int columna = 0; columna < this.extensionDeLaMatriz; columna++){
                 this.matrizDePosiciones[fila][columna] = new Posicion(extremoXNegativo + columna, extremoYPositivo - fila);
             }
         }
 
-        this.yDelPersonaje = (extensionDeLaMatriz / 2);
-        this.xDelPersonaje = (extensionDeLaMatriz / 2);
+        this.yDelPersonaje = (this.extensionDeLaMatriz / 2);
+        this.xDelPersonaje = (this.extensionDeLaMatriz / 2);
     }
 
     public Posicion obtenerPosicionDelPersonaje() {
         return this.matrizDePosiciones[this.yDelPersonaje][this.xDelPersonaje];
     }
 
-    public void moverPersonajeA(Direccion unaDireccion) {
-        this.xDelPersonaje += unaDireccion.direccionEnX();
-        this.yDelPersonaje -= unaDireccion.direccionEnY();
+    public void moverPersonajeHacia(Direccion unaDireccion) {
+        this.xDelPersonaje = (this.xDelPersonaje + unaDireccion.direccionEnX()) % this.extensionDeLaMatriz;
+        this.yDelPersonaje = (this.yDelPersonaje - unaDireccion.direccionEnY()) % this.extensionDeLaMatriz;
     }
 
     //El Sector Dibujo esta compuesto por una matriz donde cada posicion este llena de 0's,
