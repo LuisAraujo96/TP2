8866a2abaa75afb75109b0e07dfe2a03a917cc5e
Branko Ivan Tintilay Tacacho <btintilay@fi.uba.ar>
2021-02-12 18:41:08 -0300

    Agrego mas pruebas a TrazoTests

--- src/main/test/edu/fiuba/algo3/modelo/TrazoTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/TrazoTest.java
@@ -8,14 +8,67 @@ import static org.junit.jupiter.api.Assertions.*;
 class TrazoTest {
 
     @Test
-    void testAgregarLinea() {
-        Trazo trazo1 = new Trazo();
-        Trazo trazo2 = new Trazo();
+    void test01DosTrazosVaciosSonIguales(){
+        Trazo primerTrazo = new Trazo();
+        Trazo segundoTrazo = new Trazo();
 
-        trazo1.agregarLinea(new Posicion(0,0), new Posicion(0,1));
+        assertEquals(primerTrazo, segundoTrazo);
+    }
 
-        trazo2.agregarLinea(new Posicion(0,1), new Posicion(0,0));
+    @Test
+    void test02TrazoConUnaLineaEsDiferenteAUnTrazoVacio(){
+        Trazo primerTrazo = new Trazo();
+        Trazo segundoTrazo = new Trazo();
 
-        assertEquals(trazo1, trazo2);
+        primerTrazo.agregarLinea(new Posicion(0,0), new Posicion(0,1));
+
+        assertNotEquals(primerTrazo, segundoTrazo);
+    }
+
+    @Test
+    void test03DosTrazosConLaMismaLineaSonIgualesIndependientementeDeLaDireccionDeLaMisma(){
+        Trazo primerTrazo = new Trazo();
+        Trazo segundoTrazo = new Trazo();
+
+        primerTrazo.agregarLinea(new Posicion(0,0), new Posicion(0,1));
+        segundoTrazo.agregarLinea(new Posicion(0,1), new Posicion(0,0));
+
+        assertEquals(primerTrazo, segundoTrazo);
+    }
+
+    @Test
+    void test03DosTrazosConLasMismaLineasSonIgualesIndependientementeDeLaDireccionYOrdenDeLasMismas(){
+        Trazo primerTrazo = new Trazo();
+        Trazo segundoTrazo = new Trazo();
+
+        primerTrazo.agregarLinea(new Posicion(0,0), new Posicion(0,1));
+        primerTrazo.agregarLinea(new Posicion(0,1), new Posicion(1,1));
+        primerTrazo.agregarLinea(new Posicion(1,1), new Posicion(1,0));
+        primerTrazo.agregarLinea(new Posicion(1,0), new Posicion(0,0));
+
+        segundoTrazo.agregarLinea(new Posicion(0,1), new Posicion(0,0));
+        segundoTrazo.agregarLinea(new Posicion(1,1), new Posicion(1,0));
+        segundoTrazo.agregarLinea(new Posicion(1,1), new Posicion(0,1));
+        segundoTrazo.agregarLinea(new Posicion(0,0), new Posicion(1,0));
+
+        assertEquals(primerTrazo, segundoTrazo);
+    }
+
+    @Test
+    void test04DosTrazosConLineasNoConectadasSonIguales(){
+        Trazo primerTrazo = new Trazo();
+        Trazo segundoTrazo = new Trazo();
+
+        primerTrazo.agregarLinea(new Posicion(0,0), new Posicion(0,1));
+        primerTrazo.agregarLinea(new Posicion(1,0), new Posicion(1,1));
+        primerTrazo.agregarLinea(new Posicion(2,0), new Posicion(2,1));
+        primerTrazo.agregarLinea(new Posicion(3,0), new Posicion(3,1));
+
+        segundoTrazo.agregarLinea(new Posicion(2,1), new Posicion(2,0));
+        segundoTrazo.agregarLinea(new Posicion(3,1), new Posicion(3,0));
+        segundoTrazo.agregarLinea(new Posicion(1,1), new Posicion(1,0));
+        segundoTrazo.agregarLinea(new Posicion(0,1), new Posicion(0,0));
+
+        assertEquals(primerTrazo, segundoTrazo);
     }
 }
