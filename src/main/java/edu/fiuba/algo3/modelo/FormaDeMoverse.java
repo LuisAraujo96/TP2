fb5bcc4be11ef801c2a2f8b7a07a82e1092e164f
LuisAraujo96 <luis_araujo96@outlook.com>
2021-02-15 03:33:38 -0300

    fix: interfaz renombrada a Movimientos

--- src/main/java/edu/fiuba/algo3/modelo/FormaDeMoverse.java
+++
@@ -1,8 +0,0 @@
-package edu.fiuba.algo3.modelo;
-
-import edu.fiuba.algo3.modelo.Posiciones.Posicion;
-import edu.fiuba.algo3.modelo.DireccionesDeMovimiento.Direccion;
-
-public interface FormaDeMoverse {
-    Posicion moverHacia(Posicion unaPosicion, Direccion unaDireccion);
-}
