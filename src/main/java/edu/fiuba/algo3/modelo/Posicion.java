8f41f66de6a5132e13b7448c8ae30a0e84b8f22d
LuisAraujo96 <luis_araujo96@outlook.com>
2021-02-15 03:29:51 -0300

    fix: la posicion utiliza los metodos renombrados de la interfaz direccion

---
+++ src/main/java/edu/fiuba/algo3/modelo/Posicion.java
@@ -0,0 +1,35 @@
+package edu.fiuba.algo3.modelo;
+
+import edu.fiuba.algo3.modelo.DireccionesDeMovimiento.Direccion;
+
+import java.util.Objects;
+
+public class Posicion {
+    private int x;
+    private int y;
+
+    public Posicion(int unaCoordenada, int otraCoordenada){
+        this.x = unaCoordenada;
+        this.y = otraCoordenada;
+    }
+
+    public Posicion obtenerPosicionHacia(Direccion unaDireccion){
+        int nuevaX = this.x + unaDireccion.cambioEnX();
+        int nuevaY = this.y + unaDireccion.cambioEnY();
+
+        return new Posicion(nuevaX, nuevaY);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Posicion posicion = (Posicion) o;
+        return x == posicion.x && y == posicion.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
