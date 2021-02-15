13c459e85a963dd439e067cb944e8c46f1a87459
LuisAraujo96 <luis_araujo96@outlook.com>
2021-02-15 03:28:51 -0300

    fix: el bloque repetidor reutiliza los metodos de la clase concreta contenedor de bloques

--- src/main/java/edu/fiuba/algo3/modelo/Bloques/BloqueRepetidor.java
+++ src/main/java/edu/fiuba/algo3/modelo/Bloques/BloqueRepetidor.java
@@ -2,30 +2,22 @@ package edu.fiuba.algo3.modelo.Bloques;
 
 import edu.fiuba.algo3.modelo.Personaje;
 
-import java.util.ArrayList;
-import java.util.function.Consumer;
+public class BloqueRepetidor implements Bloque {
 
-public class BloqueRepetidor implements Bloque, ContenedorDeBloques{
-
+    private ContenedorDeBloques contenedor;
     private int cantidadDeRepeticiones;
-    private ArrayList<Bloque> bloques;
 
     public BloqueRepetidor(int unaCantidad){
+        this.contenedor = new ContenedorDeBloques();
         this.cantidadDeRepeticiones = unaCantidad;
-        this.bloques = new ArrayList<>();
     }
 
     @Override
     public void ejecutarSobre(Personaje personaje){
-        Consumer<Bloque> ejecutar = bloque -> bloque.ejecutarSobre(personaje);
-
         for (int iteracion = 0; iteracion < cantidadDeRepeticiones; ++iteracion) {
-            bloques.forEach(ejecutar);
+            this.contenedor.ejecutarBloquesCon(personaje);
         }
     }
 
-    @Override
-    public void agregarBloque(Bloque unBloque) {
-        bloques.add(unBloque);
-    }
+    public void agregarBloque(Bloque unBloque) { this.contenedor.agregarBloque(unBloque); }
 }
