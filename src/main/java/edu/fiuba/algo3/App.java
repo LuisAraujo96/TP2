d6f328251ac0da8a209980fd9068ac6d5121ff59
Bastian1998 <larroyo@fi.uba.ar>
2021-02-15 20:10:29 -0300

    Feat : Testing Interfaz

--- src/main/java/edu/fiuba/algo3/App.java
+++ src/main/java/edu/fiuba/algo3/App.java
@@ -2,9 +2,14 @@ package edu.fiuba.algo3;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+import javafx.geometry.Insets;
 
 /**
  * JavaFX App
@@ -13,11 +18,28 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
+        stage.setTitle("Algo3 - TP2");
 
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+        Label etiquetaBloques = new Label();
+        etiquetaBloques.setText("Sector Bloques");
+        Label etiquetaAlgoritmos = new Label();
+        etiquetaAlgoritmos.setText("Sector Algoritmos");
+        Label etiquetaDibujo = new Label();
+        etiquetaDibujo.setText("Sector Dibujo");
+
+        VBox contenedorSectorBloques = new VBox(etiquetaBloques);
+        contenedorSectorBloques.setSpacing(10);
+        VBox contenedorSectorAlgoritmo = new VBox(etiquetaAlgoritmos);
+        contenedorSectorAlgoritmo.setSpacing(10);
+        VBox contenedorSectorDibujo = new VBox(etiquetaDibujo);
+        contenedorSectorDibujo.setSpacing(10);
+
+        HBox contenedorPrincipal = new HBox(contenedorSectorBloques, contenedorSectorAlgoritmo, contenedorSectorDibujo);
+        contenedorPrincipal.setSpacing(10);
+        contenedorPrincipal.setPadding(new Insets(20));
+
+        Scene scene = new Scene(contenedorPrincipal,500, 700);
+
         stage.setScene(scene);
         stage.show();
     }
