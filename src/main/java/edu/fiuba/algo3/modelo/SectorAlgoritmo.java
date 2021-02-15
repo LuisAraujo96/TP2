13597afb92382f859cc48f725d4ac39c04690e8b
nicosanmartin <nicolassanmartin@live.com>
2021-02-15 20:38:14 -0300

    Cambio en SectorAlgoritmo

--- src/main/java/edu/fiuba/algo3/modelo/SectorAlgoritmo.java
+++ src/main/java/edu/fiuba/algo3/modelo/SectorAlgoritmo.java
@@ -10,7 +10,8 @@ public class SectorAlgoritmo extends ContenedorDeBloques{
         this.contenedor = new ContenedorDeBloques();
     }
 
-  //  public void ejecutar(Personaje personaje){ this.contenedor.ejecutar(personaje); }
+    public void ejecutarPrograma (Personaje personaje) { this.ejecutar(bloque -> bloque.ejecutarSobre(personaje)); }
+
 
     public void agregarBloque(Bloque unBloque) { this.contenedor.agregarBloque(unBloque); }
 }
