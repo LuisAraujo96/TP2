c72c6cff7aff3a6c9e7aa15b3f948a65867fd735
LuisAraujo96 <luis_araujo96@outlook.com>
2021-02-15 03:19:55 -0300

    fix: se cambiaron los nombres de los metodos de la interfaz Direccion y de todas las clases que la implementan

--- src/main/java/edu/fiuba/algo3/modelo/DireccionesDeMovimiento/DireccionAbajo.java
+++ src/main/java/edu/fiuba/algo3/modelo/DireccionesDeMovimiento/DireccionAbajo.java
@@ -3,8 +3,8 @@ package edu.fiuba.algo3.modelo.DireccionesDeMovimiento;
 public class DireccionAbajo implements Direccion{
 
     @Override
-    public int obtenerColumnaDelPersonaje() { return 0; }
+    public int cambioEnX() { return 0; }
 
     @Override
-    public int obtenerFilaDelPersonaje() { return -1; }
+    public int cambioEnY() { return -1; }
 }
