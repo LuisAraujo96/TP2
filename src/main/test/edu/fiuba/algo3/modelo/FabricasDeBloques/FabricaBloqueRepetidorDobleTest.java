1d3e36c26f73b5f385e32f6337b8742f2c9819a6
Branko Ivan Tintilay Tacacho <btintilay@fi.uba.ar>
2021-02-16 17:26:47 -0300

    Agrego Prueba de FabricaBloqueRepetidorDoble

---
+++ src/main/test/edu/fiuba/algo3/modelo/FabricasDeBloques/FabricaBloqueRepetidorDobleTest.java
@@ -0,0 +1,32 @@
+package edu.fiuba.algo3.modelo.FabricasDeBloques;
+
+import edu.fiuba.algo3.modelo.Bloques.BloqueArriba;
+import edu.fiuba.algo3.modelo.Bloques.BloqueRepetidor;
+import edu.fiuba.algo3.modelo.HerramientasDeDibujo.Lapiz;
+import edu.fiuba.algo3.modelo.Personaje;
+import edu.fiuba.algo3.modelo.Posicion;
+import edu.fiuba.algo3.modelo.SectorDibujo;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FabricaBloqueRepetidorDobleTest {
+
+    @Test
+    public void testFabricaBloquerepetidorDobleCreaUnBLoqueRepetidorDeDosRepeticiones(){
+        Personaje personaje = new Personaje(new Lapiz(new SectorDibujo()));
+        FabricaDeBloques fabrica = new FabricaBloqueRepetidorDoble();
+        BloqueRepetidor bloque = (BloqueRepetidor) fabrica.crearBloque();
+
+        bloque.agregarBloque(new BloqueArriba());
+
+        bloque.ejecutarSobre(personaje);
+
+        assertEquals(personaje.obtenerPosicion(), new Posicion(0,2));
+
+        bloque.ejecutarInversoSobre(personaje);
+
+        assertEquals(personaje.obtenerPosicion(), new Posicion(0,0));
+    }
+
+}
