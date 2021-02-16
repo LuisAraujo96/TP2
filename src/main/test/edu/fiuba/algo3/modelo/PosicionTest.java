24f91e6efbcfa4dda3ddfc17aead4ca9e8fd2011
nicosanmartin <nicolassanmartin@live.com>
2021-02-15 22:14:18 -0300

    Tests para BloqueLapizAbajo, BloqueLapizArriba y PosicionTest

--- src/main/test/edu/fiuba/algo3/modelo/PosicionTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/PosicionTest.java
@@ -1,5 +1,9 @@
 package edu.fiuba.algo3.modelo;
 
+import edu.fiuba.algo3.modelo.DireccionesDeMovimiento.DireccionAbajo;
+import edu.fiuba.algo3.modelo.DireccionesDeMovimiento.DireccionArriba;
+import edu.fiuba.algo3.modelo.DireccionesDeMovimiento.DireccionDerecha;
+import edu.fiuba.algo3.modelo.DireccionesDeMovimiento.DireccionIzquierda;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,17 +17,47 @@ class PosicionTest {
 
         assertEquals(unaPosicion, otraPosicion);
     }
-/*
+
     @Test
-    public void test02UnaPosicionPintadaYOtraSinPintarConLasMismasCoordenadasNoSonIguales(){
+    public void test02ObtenerPosicionHaciaDerechaDesdeLaCoordenadaCeroCeroDebeDevolverCoordenadaUnoCero(){
         Posicion unaPosicion = new Posicion(0, 0);
-        Posicion otraPosicion = new Posicion(0, 0);
+        Posicion posicionALaDerecha = new Posicion(1, 0);
 
-        unaPosicion.dibujar();
+        Posicion posicionObtenida = unaPosicion.obtenerPosicionHacia(new DireccionDerecha());
 
-        assertNotEquals(unaPosicion, otraPosicion);
+        assertEquals(posicionALaDerecha, posicionObtenida);
     }
 
+    @Test
+    public void test03ObtenerPosicionHaciaIzquierdaDesdeLaCoordenadaCeroCeroDebeDevolverCoordenadaMenosUnoCero(){
+        Posicion unaPosicion = new Posicion(0, 0);
+        Posicion posicionALaIzquierda = new Posicion(-1, 0);
+
+        Posicion posicionObtenida = unaPosicion.obtenerPosicionHacia(new DireccionIzquierda());
+
+        assertEquals(posicionALaIzquierda, posicionObtenida);
+    }
+
+    @Test
+    public void test04ObtenerPosicionHaciaArribaDesdeLaCoordenadaCeroCeroDebeDevolverCoordenadaCeroUno(){
+        Posicion unaPosicion = new Posicion(0, 0);
+        Posicion posicionArriba = new Posicion(0, 1);
+
+        Posicion posicionObtenida = unaPosicion.obtenerPosicionHacia(new DireccionArriba());
+
+        assertEquals(posicionArriba, posicionObtenida);
+    }
+    @Test
+    public void test05ObtenerPosicionHaciaAbajoDesdeLaCoordenadaCeroCeroDebeDevolverCoordenadaCeroMenosUno(){
+        Posicion unaPosicion = new Posicion(0, 0);
+        Posicion posicionAbajo = new Posicion(0, -1);
+
+        Posicion posicionObtenida = unaPosicion.obtenerPosicionHacia(new DireccionAbajo());
+
+        assertEquals(posicionAbajo, posicionObtenida);
+    }
+
+    /*
     @Test
     public void test03RepresentarUnaPosicionRecienCreadaDevuelveComoResultado0(){
         Posicion unaPosicion = new Posicion(0, 0);
