df471f3bf6aa539a7c4fdb56f9f6e5c06abaeb40
LuisAraujo96 <luis_araujo96@outlook.com>
2021-02-16 02:34:31 -0300

    feat: se agrega una interfaz para la creacion de las fabricas de bloques

--- src/main/java/edu/fiuba/algo3/modelo/FabricasDeBloques/FabricaDeBloques.java
+++ src/main/java/edu/fiuba/algo3/modelo/FabricasDeBloques/FabricaDeBloques.java
@@ -2,7 +2,7 @@ package edu.fiuba.algo3.modelo.FabricasDeBloques;
 
 import edu.fiuba.algo3.modelo.Bloques.Bloque;
 
-abstract class FabricaDeBloques {
+public interface FabricaDeBloques {
 
-    abstract Bloque crearBloque();
+    Bloque crearBloque();
 }
