7e0114f0220684ead53b5b174d58087641db25e7
Branko Ivan Tintilay Tacacho <btintilay@fi.uba.ar>
2021-02-16 23:34:43 -0300

    Agrego Prueba de FabricaBloqueDerecha

---
+++ src/main/test/edu/fiuba/algo3/modelo/FabricasDeBloquesTest/FabricaBloqueDerechaTest.java
@@ -0,0 +1,31 @@
+package edu.fiuba.algo3.modelo.FabricasDeBloquesTest;
+
+import edu.fiuba.algo3.modelo.Bloques.Bloque;
+import edu.fiuba.algo3.modelo.FabricasDeBloques.FabricaBloqueDerecha;
+import edu.fiuba.algo3.modelo.FabricasDeBloques.FabricaDeBloques;
+import edu.fiuba.algo3.modelo.HerramientasDeDibujo.Lapiz;
+import edu.fiuba.algo3.modelo.Personaje;
+import edu.fiuba.algo3.modelo.Posicion;
+import edu.fiuba.algo3.modelo.SectorDibujo;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FabricaBloqueDerechaTest {
+
+    @Test
+    public void testFabricaBloqueDerechaCreaUnBLoqueDerecha(){
+        Personaje personaje = new Personaje(new Lapiz(new SectorDibujo()));
+        FabricaDeBloques fabrica = new FabricaBloqueDerecha();
+        Bloque bloque = fabrica.crearBloque();
+
+        bloque.ejecutarSobre(personaje);
+
+        assertEquals(personaje.obtenerPosicion(), new Posicion(1,0));
+
+        bloque.ejecutarInversoSobre(personaje);
+
+        assertEquals(personaje.obtenerPosicion(), new Posicion(0,0));
+    }
+
+}
