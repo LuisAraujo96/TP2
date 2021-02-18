741964a956c806f9b39cbc4741aab2684cf76e43
Bastian1998 <larroyo@fi.uba.ar>
2021-02-17 21:53:39 -0300

    feat(contenedores): bloques complejos y choiceBox

---
+++ src/main/java/edu/fiuba/algo3/vista/componentes/contenedores/ContenedorBloqueComplejo.java
@@ -0,0 +1,36 @@
+package edu.fiuba.algo3.vista.componentes.contenedores;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+
+public class ContenedorBloqueComplejo extends VBox {
+    Button agregar;
+    Button limpiar;
+    ChoiceBox<String> choiceBox;
+    public ContenedorBloqueComplejo(){
+
+        choiceBox = new ChoiceBox<>();
+        choiceBox.getItems().add("Bloque Repetidor");
+        choiceBox.getItems().add("Bloque Inversor");
+        choiceBox.getItems().add("Bloque Algoritmo");
+        choiceBox.setPrefSize(160,20);
+
+        agregar = new Button("Agregar");
+        limpiar = new Button("Limpiar");
+
+        HBox miniContenedorBotones = new HBox(agregar, limpiar);
+        miniContenedorBotones.setSpacing(10);
+        miniContenedorBotones.setAlignment(Pos.BASELINE_CENTER);
+
+        this.setId("ContenedorBloquesComplejos");
+        this.setSpacing(10);
+        this.setAlignment(Pos.BASELINE_CENTER);
+        this.setPadding(new Insets(10));
+
+        this.getChildren().addAll(choiceBox, miniContenedorBotones);
+    }
+
+}
