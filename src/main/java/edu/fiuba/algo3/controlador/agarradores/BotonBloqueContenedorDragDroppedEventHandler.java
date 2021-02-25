4b1ac524e24a00553002caf6c660cc05d6ecc914
LuisAraujo96 <luis_araujo96@outlook.com>
2021-02-24 23:14:31 -0300

    feat: se agrega funcionalidad a los controladores de drag and drop

--- src/main/java/edu/fiuba/algo3/controlador/agarradores/BotonBloqueContenedorDragDroppedEventHandler.java
+++ src/main/java/edu/fiuba/algo3/controlador/agarradores/BotonBloqueContenedorDragDroppedEventHandler.java
@@ -1,5 +1,7 @@
 package edu.fiuba.algo3.controlador.agarradores;
 
+import edu.fiuba.algo3.modelo.Bloques.*;
+import edu.fiuba.algo3.modelo.ContenedorDeBloques;
 import javafx.event.EventHandler;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.DragEvent;
@@ -8,10 +10,12 @@ import javafx.scene.layout.VBox;
 
 public class BotonBloqueContenedorDragDroppedEventHandler implements EventHandler<DragEvent> {
     private VBox contenedorDeBloquesVertical;
+    private ContenedorDeBloques bloqueContenedor;
     private SeccionAlgoritmoDragDroppedEventHandler eventoContenedorHorizontalDeBloques;
 
-    public BotonBloqueContenedorDragDroppedEventHandler(VBox contenedorDeBloquesVertical, SeccionAlgoritmoDragDroppedEventHandler eventoContenedorHorizontalDeBloques){
+    public BotonBloqueContenedorDragDroppedEventHandler(VBox contenedorDeBloquesVertical, ContenedorDeBloques bloqueContenedor, SeccionAlgoritmoDragDroppedEventHandler eventoContenedorHorizontalDeBloques){
         this.contenedorDeBloquesVertical = contenedorDeBloquesVertical;
+        this.bloqueContenedor = bloqueContenedor;
         this.eventoContenedorHorizontalDeBloques = eventoContenedorHorizontalDeBloques;
     }
 
@@ -27,6 +31,8 @@ public class BotonBloqueContenedorDragDroppedEventHandler implements EventHandle
             ImageView imageView = new ImageView(db.getImage());
             this.contenedorDeBloquesVertical.getChildren().add(imageView);
 
+            this.agregarUnBloqueAlBloqueContenedor(db.getString());
+
             this.eventoContenedorHorizontalDeBloques.agregueDesdeUnContenedorComplejo();
 
             success = true;
@@ -34,6 +40,28 @@ public class BotonBloqueContenedorDragDroppedEventHandler implements EventHandle
         /* let the source know whether the string was successfully
          * transferred and used */
         dragEvent.setDropCompleted(success);
+    }
 
+    public void agregarUnBloqueAlBloqueContenedor(String bloque_id){
+
+        if (bloque_id == "BotonMoverArriba"){
+            this.bloqueContenedor.agregarBloque(new BloqueArriba());
+        } else if (bloque_id == "BotonMoverAbajo"){
+            this.bloqueContenedor.agregarBloque(new BloqueAbajo());
+        } else if (bloque_id == "BotonMoverIzquierda"){
+            this.bloqueContenedor.agregarBloque(new BloqueIzquierda());
+        } else if (bloque_id == "BotonMoverDerecha"){
+            this.bloqueContenedor.agregarBloque(new BloqueDerecha());
+        } else if (bloque_id == "BotonSubirLapiz"){
+            this.bloqueContenedor.agregarBloque(new BloqueLapizArriba());
+        } else if (bloque_id == "BotonBajarLapiz"){
+            this.bloqueContenedor.agregarBloque(new BloqueLapizAbajo());
+        } else if (bloque_id == "BotonBloqueInversor"){
+            this.bloqueContenedor.agregarBloque(new BloqueInversor());
+        } else if (bloque_id == "BotonRepetidorDoble"){
+            this.bloqueContenedor.agregarBloque(new BloqueRepetidor(2));
+        } else if (bloque_id == "BotonRepetidorTriple"){
+            this.bloqueContenedor.agregarBloque(new BloqueRepetidor(3));
+        }
     }
 }
--- src/main/java/edu/fiuba/algo3/controlador/agarradores/SeccionAlgoritmoDragDroppedEventHandler.java
+++ src/main/java/edu/fiuba/algo3/controlador/agarradores/SeccionAlgoritmoDragDroppedEventHandler.java
@@ -1,5 +1,7 @@
 package edu.fiuba.algo3.controlador.agarradores;
 
+import edu.fiuba.algo3.modelo.Bloques.*;
+import edu.fiuba.algo3.modelo.ContenedorDeBloques;
 import javafx.event.EventHandler;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.DragEvent;
@@ -10,6 +12,7 @@ import javafx.scene.layout.VBox;
 public class SeccionAlgoritmoDragDroppedEventHandler implements EventHandler<DragEvent> {
     private HBox contenedorDeBloquesHorizontal;
     private boolean agregadoDesdeUnContenedorComplejo = false;
+    //private SectorAlgoritmo sectorAlgoritmo;
 
     public SeccionAlgoritmoDragDroppedEventHandler(HBox contenedorDeBloquesHorizontal){
         this.contenedorDeBloquesHorizontal = contenedorDeBloquesHorizontal;
@@ -26,12 +29,14 @@ public class SeccionAlgoritmoDragDroppedEventHandler implements EventHandler<Dra
 
             ImageView imageView = new ImageView(db.getImage());
 
+            Bloque bloqueCreado = this.agregarUnBloqueAlSectorAlgoritmo(db.getString());
+
             if (db.getString() == "BotonBloqueInversor" || db.getString() == "BotonRepetidorDoble" || db.getString() == "BotonRepetidorTriple"){
 
                 VBox contenedorDeBloquesVertical = new VBox(imageView);
 
                 contenedorDeBloquesVertical.setOnDragOver(new BotonBloqueContenedorDragOverEventHandler(contenedorDeBloquesVertical));
-                contenedorDeBloquesVertical.setOnDragDropped(new BotonBloqueContenedorDragDroppedEventHandler(contenedorDeBloquesVertical, this));
+                contenedorDeBloquesVertical.setOnDragDropped(new BotonBloqueContenedorDragDroppedEventHandler(contenedorDeBloquesVertical, (ContenedorDeBloques)bloqueCreado, this));
 
                 this.contenedorDeBloquesHorizontal.getChildren().add(contenedorDeBloquesVertical);
 
@@ -52,7 +57,31 @@ public class SeccionAlgoritmoDragDroppedEventHandler implements EventHandler<Dra
 
     public void agregueDesdeUnContenedorComplejo(){ this.agregadoDesdeUnContenedorComplejo = true; }
 
-    public void agregarBloqueAlSectorAlgoritmo(String bloque_id){
+    public Bloque agregarUnBloqueAlSectorAlgoritmo(String bloque_id){
 
+        Bloque bloqueCreado = null;
+
+        if (bloque_id == "BotonMoverArriba"){
+            bloqueCreado = new BloqueArriba();
+        } else if (bloque_id == "BotonMoverAbajo"){
+            bloqueCreado = new BloqueAbajo();
+        } else if (bloque_id == "BotonMoverIzquierda"){
+            bloqueCreado = new BloqueIzquierda();
+        } else if (bloque_id == "BotonMoverDerecha"){
+            bloqueCreado = new BloqueDerecha();
+        } else if (bloque_id == "BotonSubirLapiz"){
+            bloqueCreado = new BloqueLapizArriba();
+        } else if (bloque_id == "BotonBajarLapiz"){
+            bloqueCreado = new BloqueLapizAbajo();
+        } else if (bloque_id == "BotonBloqueInversor"){
+            bloqueCreado = new BloqueInversor();
+        } else if (bloque_id == "BotonRepetidorDoble"){
+            bloqueCreado = new BloqueRepetidor(2);
+        } else if (bloque_id == "BotonRepetidorTriple"){
+            bloqueCreado = new BloqueRepetidor(3);
+        }
+
+        //this.sectorAlgoritmo.agregarBloque(bloqueCreado);
+        return bloqueCreado;
     }
 }
