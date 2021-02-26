1b650a924b84a8d51922e90cc3a32ade7240da64
Branko Ivan Tintilay Tacacho <btintilay@fi.uba.ar>
2021-02-26 15:23:46 -0300

    Agrego Controlador para crear Bloques Contenedores

--- src/main/java/edu/fiuba/algo3/controlador/CreadorDeBloquesContenedoresEventHandler.java
+++ src/main/java/edu/fiuba/algo3/controlador/CreadorDeBloquesContenedoresEventHandler.java
@@ -1,21 +1,22 @@
 package edu.fiuba.algo3.controlador;
 
+import edu.fiuba.algo3.modelo.Bloques.BloqueContenedor;
 import edu.fiuba.algo3.modelo.FabricasDeBloques.FabricaDeBloques;
 import edu.fiuba.algo3.modelo.SectorAlgoritmo;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 
-public class CreadorDeBloquesEventHandler implements EventHandler<ActionEvent> {
+public class CreadorDeBloquesContenedoresEventHandler implements EventHandler<ActionEvent> {
     SectorAlgoritmo sectorAlgoritmo;
     FabricaDeBloques fabrica;
 
-    public CreadorDeBloquesEventHandler(SectorAlgoritmo unSector, FabricaDeBloques unaFabrica){
+    public CreadorDeBloquesContenedoresEventHandler(SectorAlgoritmo unSector, FabricaDeBloques unaFabrica){
         this.sectorAlgoritmo = unSector;
         this.fabrica = unaFabrica;
     }
 
     @Override
     public void handle(ActionEvent actionEvent) {
-        this.sectorAlgoritmo.agregarBloque( this.fabrica.crearBloque() );
+        this.sectorAlgoritmo.agregarBloqueContenedor( (BloqueContenedor) this.fabrica.crearBloque() );
     }
 }
